3fe36515db426b43d319307dbeb726efb7bf1cdf
Mateus Barbosa <77073090+MateusBarbosa1@users.noreply.github.com>
2024-01-30 17:57:36 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ function getTec() {
 function getAbout() {
     div.innerHTML = `<h1 style="font-weight: bold;">About me</h1>
                     <br>
-                    <p style="padding: 3px 0;">Hi, My history in the technology area begins at the age of 10 when I watched a film that talked about hackers, I thought it was incredible, it didn't take long for me to discover the area of ​​programming, I started learning Python, but it didn't take long for me to discover web development so I started studying every day and decided to focus on the back end, and it didn't take long to start selling websites to family members and entrepreneurs in my city.</p>
+                    <p style="padding: 3px 0;">Hello, My story in the technology area begins at the age of 10 when I watched a film that talked about hackers, I thought it was incredible, then I discovered programming, I started learning Python, but when I saw web development I thought it was really cool so I started studying all the days and decided to focus on the back-end, and it didn't take long to start selling websites to family members and entrepreneurs in my city.</p>
                     <p>Anyway, I believe that technology can save the world and I want to take a closer look at this event!</p>
                     <p><span style="font-weight: bold;">REMEMBERING:</span> programmers are those who solve problems, so don't get attached to one language because technology is constantly changing and at any time you will have to learn another language to better solve your problem :)</p>
                     <br>
