9ce5206b55434727239c7cc2bbdb1073136cab4b
Mateus Barbosa <pg9779628@gmail.com>
2024-03-01 14:59:01 -0300

    Change to portuguese

--- js/script.js
+++ js/script.js
@@ -1,41 +1,30 @@
-var div = document.querySelector('.info div')
-
+var div = document.querySelector(".info div");
 
 function getTec() {
-    div.innerHTML = `<h1 style="font-weight: bold;">Technologies</h1>
+  div.innerHTML = `<h1 style="font-weight: bold;">Technologies</h1>
                     <br>
                     <p style="padding: 3px 0;">JavaScript - Nodejs</p>
                     <p style="padding: 3px 0;">Databases - (Mysql,Postgresql,MongoDB)</p>
                     <p style="padding: 3px 0;">Design Patterns</p>
-                    <p style="padding: 3px 0;">Infrastructure knowledge</p>
-                    <p style="padding: 3px 0;">Java - (Learning)</p>
+                    <p style="padding: 3px 0;">Conhecimentos de Infraestrutura</p>
+                    <p style="padding: 3px 0;">Java - (Aprendendo)</p>
                     <p style="padding: 3px 0;">HTML/CSS</p>
                     <br>
-                    <h1 style="font-size: 20px;font-weight: bold;">Main Frameworks</h1>
-                    <br>
-                    <p style="padding: 3px 0;">Express</p>
-                    <p style="padding: 3px 0;">Next JS</p>
-                    <p style="padding: 3px 0;">Prisma</p>
-                    <p style="padding: 3px 0;">Body-Parser</p>
-                    <p style="padding: 3px 0;">Consign</p>
-                    <br>
-                    <button onclick="home()" ><a>Home</a></button>`
+                    <button onclick="home()" ><a>Home</a></button>`;
 }
 function getAbout() {
-    div.innerHTML = `<h1 style="font-weight: bold;">About me</h1>
+  div.innerHTML = `<h1 style="font-weight: bold;">Sobre mim</h1>
                     <br>
-                    <p style="padding: 3px 0;">Hello, My story in the technology area begins at the age of 10 when I watched a film that talked about hackers, I thought it was incredible, then I discovered programming, I started learning Python, but when I saw web development I thought it was really cool so I started studying all the days and decided to focus on the back-end, and it didn't take long to start selling websites to family members and entrepreneurs in my city.</p>
-                    <p>Anyway, I believe that technology can save the world and I want to take a closer look at this event!</p>
-                    <p><span style="font-weight: bold;">REMEMBERING:</span> programmers are those who solve problems, so don't get attached to one language because technology is constantly changing and at any time you will have to learn another language to better solve your problem :)</p>
+                    <p style="padding: 3px 0;">Olá, Minha história na área de tecnologia começa aos 10 anos quando assisti um filme que falava sobre hackers, achei incrível, depois descobri programação, comecei a aprender Python, mas quando vi desenvolvimento web achei que era muito legal então comecei a estudar todos os dias e resolvi focar no back-end, e não demorou muito para começar a vender sites para familiares e empreendedores da minha cidade.</p>
                     <br>
-                    <button onclick="home()" ><a>Home</a></button>`
+                    <button onclick="home()" ><a>Home</a></button>`;
 }
 function home() {
-    div.innerHTML = `<h1 id="title1">Welcome!</h1>
+  div.innerHTML = `<h1 id="title1">Welcome!</h1>
     <br>
     <p id="p1">I'm a back-end developer who works mainly with nodejs!</p>
     <br>
-    <button><a href="/projects.html">View Projects</a></button>
-    <button onclick="getTec()"><a>Technical knowledge</a></button>
-    <button onclick="getAbout()"><a>About me</a></button>`
+    <button><a href="/projects.html">Ver Projetos</a></button>
+    <button onclick="getTec()"><a>Conhecimentos Tecnicos</a></button>
+    <button onclick="getAbout()"><a>Sobre mim</a></button>`;
 }
